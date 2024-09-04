56f085eb6d9df4099f480a372b5aa1e95e27f7fc
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-09-04 12:52:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+let data = [
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." }]
+
+
 function App() {
+  const [ip,setIp] = useState('');
+  const [res,setRes] = useState("");
+  const handleSubmit = (e)=>{
+     e.preventDefault();
+     let findData = data.find((ele)=>{
+      return ele.word.toLowerCase() === ip.toLowerCase();
+     })
+     console.log(findData);
+     setRes(findData)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <form onSubmit={handleSubmit}> 
+        <input value={ip} onChange={(e)=>setIp(e.target.value)} />
+        <button type='submit'>Submit</button>
+      </form>
+      <h5>Definition:</h5>
+      {res && res.meaning?<p>{res.meaning}</p>:""}
+      {res===""?"":<p>Word not found in the dictionary.</p>}
     </div>
   );
 }
